85717fcd7c8a12f9a4741044a78c30551217566c
Bernard Aboba <bernard.aboba@gmail.com>
2020-07-01 00:04:10 -0700

    Get basic files in place
    Add basic files to the repo.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# webtransport-use-cases
+# nv-use-cases
+Webtransport Use Cases
+
+This document describes a set of use cases motivating the development of
+Webtransport.
+
+### Published Versions
+
+The editors' draft is the tip-of-tree of this document, it may contain work in
+progress changes and other inconsistencies, including an incomplete change log.
+
+* [Latest editor's draft at github](https://w3c.github.io/webtransport-use-cases/)
+
+### Useful Links
+
+The content of this document is discussed at the
+[public-webrtc](https://lists.w3.org/Archives/Public/public-webrtc/)
+mailing list.
+
+[Contribution Guidelines](CONTRIBUTING.md)
---
+++ respec-config.js
@@ -0,0 +1,65 @@
+var respecConfig = {
+  specStatus: "ED",
+  // if there a publicly available Editor's Draft, this is the link
+  edDraftURI: "https://w3c.github.io/webtranpsort-use-cases/",
+  shortName: "webtransport-use-cases",
+  editors: [
+    { name: "Bernard Aboba", company: "Microsoft Corporation", w3cid: "65611" }
+  ],
+  authors: [
+  ],
+  wg: "Webtransport Working Group",
+  wgURI: "https://www.w3.org/2011/04/webrtc/",
+  wgPublicList: "public-webrtc",
+  wgPatentURI:  "https://www.w3.org/2004/01/pp-impl/47318/status",
+  issueBase: "https://github.com/w3c/webrtc-nv-use-cases/issues",
+  otherLinks: [
+    {
+      key: "Participate",
+      data: [
+        {
+          value: "Mailing list",
+          href: "https://lists.w3.org/Archives/Public/public-webrtc/"
+        },
+        {
+          value: "Browse open issues",
+          href: "https://github.com/w3c/webtransport-use-cases/issues"
+        }
+      ]
+    }
+  ],
+  localBiblio: {
+    "IANA-STUN-6": {
+      "title": "STUN Error Codes",
+      "href": "https://www.iana.org/assignments/stun-parameters/stun-parameters.xhtml#stun-parameters-6",
+      "publisher": "IANA"
+    },
+      "ORTC": {
+      "title": "Object RTC (ORTC) API for WebRTC",
+      "href": "https://w3c.github.io/ortc/",
+      "authors": [
+        "Robin Raymond"
+      ],
+      "editors": [
+        "Bernard Aboba",
+        "Justin Uberti"
+      ],
+      "status": "03 October 2018 (work in progress)",
+      "publisher": "W3C"
+    },
+      "MLS-ARCH": {
+      "title": "The Messaging Layer Security (MLS) Architecture",
+      "href": "https://tools.ietf.org/html/draft-ietf-mls-architecture",
+      "authors": [
+        "E. Omara",
+        "B. Beurdouche",
+        "E. Rescorla",
+        "S. Inguva",
+        "A. Kwon",
+        "A. Duric"
+      ],
+      "status": "11 March 2019. Internet Draft (work in progress)",
+      "publisher": "IETF"
+    },
+  }
+}
